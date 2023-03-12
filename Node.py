3426527a6a3aafbb7e94e4f69b72e426cd819ba5
Joseph <112746230+josephgit10@users.noreply.github.com>
2023-03-12 14:31:17 -0500

    Add files via upload

---
+++ Node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, pos, cost, parent):
+        self.pos = pos                      # Position of the node
+        self.x = pos[0]                     # x-coordinate of the node
+        self.y = pos[1]                     # y-coordinate of the node
+        self.cost = cost                    # Total cost to reach the node 
+        self.parent = parent                # Parent of the node
